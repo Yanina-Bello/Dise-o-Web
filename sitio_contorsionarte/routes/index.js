724fd44b842d919d8270303f3f0f679df6048a12
Yanina Bello <yaninabello@live.com>
2023-09-19 17:14:58 -0300

    sitio_contorsionarte

---
+++ sitio_contorsionarte/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+/* GET home page. */
+router.get('/', function (req, res, next) 
+res.render('index', {
+  message: 'Mensaje enviado correctamente'
+  
+router.post('/', async (req, res, next) =>{
+
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'yaninabello@live.com',
+    subject: 'contacto web',
+    html: nombre + "Se contactó a través de la web y quiere más información a este correo: " + email + ". <br> Su tel es: " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  var info = await transport.sendMail(obj);
+
+  });
+
+});
