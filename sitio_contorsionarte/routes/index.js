2d3b6041af0118b8e235009a7ca34fc0c446503f
Yanina Bello <yaninabello@live.com>
2023-09-22 17:54:13 -0300

    229

--- sitio_contorsionarte/routes/index.js
+++ sitio_contorsionarte/routes/index.js
@@ -4,9 +4,7 @@ var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    message: 'Mensaje enviado correctamente'
-  });
+
 });
 
 router.post('/', async (req, res, next) => {
@@ -23,17 +21,19 @@ router.post('/', async (req, res, next) => {
   }
 
   var transport = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-      }
-    });
-
-    var info = await transport.sendMail(obj);
-
-    module.exports = router;
-
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
   });
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  });
+
+});
 
