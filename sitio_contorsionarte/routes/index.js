5658bb410504f15f114ff3d2624442ba8741e00a
Yanina Bello <yaninabello@live.com>
2023-09-22 17:43:58 -0300

    ultimo intento22-09

--- sitio_contorsionarte/routes/index.js
+++ sitio_contorsionarte/routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
-router.get('/', function (req, res, next) 
-res.render('index', {
-  message: 'Mensaje enviado correctamente'
-  
-router.post('/', async (req, res, next) =>{
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+router.post('/', async (req, res, next) => {
 
   var nombre = req.body.nombre;
   var email = req.body.email;
@@ -17,20 +19,21 @@ router.post('/', async (req, res, next) =>{
   var obj = {
     to: 'yaninabello@live.com',
     subject: 'contacto web',
-    html: nombre + "Se contactó a través de la web y quiere más información a este correo: " + email + ". <br> Su tel es: " + tel
+    html: nombre + "Se contactó a través de la web y quiere más información a este correo: " + email + ". <br> Su tel es: " + tel + mensaje
   }
 
   var transport = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    }
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    var info = await transport.sendMail(obj);
+
+    module.exports = router;
+
   });
 
-  var info = await transport.sendMail(obj);
-
-  });
-
-});
