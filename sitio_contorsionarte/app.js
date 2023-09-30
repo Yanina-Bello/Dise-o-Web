3583677de30c48101d768c2637d9d36dbf1d9089
Yanina Bello <yaninabello@live.com>
2023-09-30 19:06:59 -0300

    finalmodulo4

--- sitio_contorsionarte/app.js
+++ sitio_contorsionarte/app.js
@@ -5,12 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
-
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var loginRouter = require('./routes/admin/login');
 var app = express();
-
+var adminRouter = require('./routes/admin/novedades');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,18 +23,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/admin/login/login', loginRouter);
+app.use('/admin/novedades', adminRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
+secured = async (req, res, next) => {
+  try {
+    console.log(req.session.id_usuario);
+    if (req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect('/admin/login');
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+
+}
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+app.use('/admin/novedades',secured, adminNovedadesRouter);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
